614fbeffa01447e054ee457b4913f3d0b6797b32
Dhruv Hegde <desidhruv@gmail.com>
2017-10-19 17:15:02 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,22 +1,27 @@
 function forLoop(array){
-for(int i=0;i<25;i++){
-if(i==1){
+for(let i=0;i<25;i++){
+if(i===1){
 array.push('I am 1 strange loop');
 } else {
 array.push('I am ${i} strange loop');
 }
 }
+return array;
 }
 
-function whileLoop(n){
-let countdown = n;
-while(countdown > 0){
-countdown--;
+function whileLoop(number){
+while(number > 0){
+number--;
 }
 if(countdown == 0){
 return 'done';
 }
+return 'done';
 }
+
+function maybeTrue() {
+ +  return Math.random() >= 0.5
+ +}
 
 function doWhileLoop(array){
 while(array.length>0 && maybeTrue()){
